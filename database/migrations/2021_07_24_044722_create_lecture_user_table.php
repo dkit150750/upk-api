0e2128e633cc753a5f57c490bdcdd8b94c3116b7
dkit150750 <dkit150750@gmail.com>
2021-08-07 15:49:01 +0300

    Fix

--- database/migrations/2021_07_24_044722_create_lecture_user_table.php
+++ database/migrations/2021_07_24_044722_create_lecture_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserLectureTable extends Migration
+class CreateLectureUserTable extends Migration
 {
     /**
      * Run the migrations.
